2f0b06a5e520b0595e545741a7d00679b03746d7
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-05-11 13:40:10 -0500

    Fix linter issue

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@
 
 if ENV["SENTRY_DNS"].present?
   Sentry.init do |config|
-    config.dsn = ENV["SENTRY_DNS"]
+    config.dsn = ENV.fetch("SENTRY_DNS", nil)
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
     config.traces_sample_rate = 0.85
   end
